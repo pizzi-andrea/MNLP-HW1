a4f1dea18e81938b2c5c294c474dbcd18da5b7fc
Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>
2025-04-21 09:03:29 +0200

    fix all implementative bugs(syntax errors)
    Signed-off-by: Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>

---
+++ src/PyDataset.py
@@ -0,0 +1,37 @@
+from typing import override, Any
+import torch
+import numpy as np
+import pandas as pd
+from pathlib import PosixPath
+class PyDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset_file:PosixPath, features:list[str], target_label:str) -> None:
+        super().__init__()
+        
+        self._file = dataset_file
+        self._target = target_label
+        self._sep = ''
+
+        if self._file.exists():
+            if self._file.suffix == '.csv':
+                self._sep  = ',' 
+            elif self._file.suffix == '.tsv':
+                self._sep = '\t'
+            else:
+                raise Exception('Dataset format not valid, save data in follow formats: [csv, tsv] ')
+        else:
+            raise FileNotFoundError(f'File {self._file} not found')
+        
+        self._data = pd.read_csv(self._file, sep=self._sep)
+        
+        self._y = self._data[self._target].copy(deep=True).astype(np.float32).to_numpy()
+        
+        self._X = self._data.drop(self._target, axis=1)[features].astype(np.int64).to_numpy()
+    
+    
+    def __len__(self) -> int:
+        return self._y.size
+    
+    @override
+    def __getitem__(self, index) -> tuple[Any, Any]:
+        return (self._X[index], self._y[index])
+    
