75711b4bf8194ea912ca8842ad760774e124989e
Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>
2025-04-23 12:20:16 +0200

    restructure init for simplify utils methods, add dataset in local cache, review some methods signature

--- src/CU_Dataset_Factory.py
+++ src/CU_Dataset_Factory.py
@@ -2,12 +2,12 @@ import pandas as pd
 import pathlib as path
 import os
 from sklearn.preprocessing import LabelEncoder
+from requests import get
 from tqdm import tqdm
 from datasets import load_dataset
 from utils import batch_generator
 from features import count_references, dominant_langs, langs_length, G_factor
 from Connection import Wiki_high_conn
-
 
 # Class to append the new features to the dataset and produce the new dataset
 class CU_Dataset_Factory:
@@ -16,47 +16,93 @@ class CU_Dataset_Factory:
     """
     def __init__(
         self,
-        target_feature: str,
-        batch_size: int = 1,
-        features_enable: list[str] = ["label"],
+        load:bool = False,
     ) -> None:
 
-        self.batch_size = batch_size
-        self.features_enable = features_enable
-        self.conn = Wiki_high_conn()
-        self.features_enable.sort()
-        self.label_e = LabelEncoder()
-        self.__gf = {"G_mean_pr", "G_nodes", "G_num_cliques", "G_rank", "G_avg", 'G_diameter'}
-        self._id = {'item', 'name'}
-        if not target_feature in features_enable:
-            raise ValueError("target feature must be in features_enable_")
-        self.target = target_feature
-        pd.set_option("mode.chained_assignment", None)
 
-        self.train: pd.DataFrame = load_dataset("sapienzanlp/nlp2025_hw1_cultural_dataset", cache_dir="dataset")["train"].to_pandas()  # type: ignore
-        self.validation: pd.DataFrame = load_dataset("sapienzanlp/nlp2025_hw1_cultural_dataset", cache_dir="dataset")["validation"].to_pandas()  # type: ignore
+        def __wiki_name(qids: list[str]) -> dict[str, str]:
+            def fetch_batch(qids_batch: list[str]) -> dict[str, str]:
+                # Parametri per l'API di Wikidata
+                params = {
+                    'action': 'wbgetentities',
+                    'ids': '|'.join(qids_batch),
+                    'sites': 'wikipedia',
+                    'props': 'sitelinks',
+                    'format': 'json',
+                    'utf8': 1
+                }
+           
+                data = get('https://www.wikidata.org/w/api.php', params).json().get('entities', {})
+                batch_map = {}
+                for qid, entity in data.items():
+                    sitelinks = entity.get('sitelinks', {})
+                    lang_key = f"enwiki"  # es. "enwiki"
+                    title = sitelinks.get(lang_key, {}).get('title', '')
+                    batch_map[qid] = title
+                return batch_map
+
+            qid_to_title: dict[str, str] = {}
+            batch_size = 50
+            for i in range(0, len(qids), batch_size):
+                batch = qids[i:i + batch_size]
+                qid_to_title.update(fetch_batch(batch))
+
+            return qid_to_title
+
+        self._cache = path.PosixPath('./_cache')
+        self.conn = Wiki_high_conn()
+        self.label_e = LabelEncoder()
+        self.sgf = {"G_mean_pr", "G_nodes", "G_num_cliques", "G_avg"}
+        self.pgf = {'languages', 'reference'}
+        self.pef = {'n_mod', 'n_visits'}
+        self.id = {'qid', 'wiki_name'}
+        pd.set_option("mode.chained_assignment", None)
+        if not self._cache.exists():
+            os.mkdir(self._cache)
+        
+        if load and self._cache.exists() and self._cache.joinpath('base_train.csv').exists() and self._cache.joinpath('base_validation.csv').exists():
+            self.validation = pd.read_csv(self._cache.joinpath('base_validation.csv'), sep='\t')
+            self.train = pd.read_csv(self._cache.joinpath('base_train.csv'), sep='\t')
+
+            self.train = self.train.drop('Unnamed: 0', axis=1)
+            self.validation = self.validation.drop('Unnamed: 0', axis=1)
+        else:
+            self.train: pd.DataFrame = load_dataset("sapienzanlp/nlp2025_hw1_cultural_dataset", cache_dir="dataset")["train"].to_pandas()  # type: ignore
+            self.validation: pd.DataFrame = load_dataset("sapienzanlp/nlp2025_hw1_cultural_dataset", cache_dir="dataset")["validation"].to_pandas()  # type: ignore
+            self.train['qid'] = self.train['item'].str.extract(r'(Q\d+)', expand=False)
+            self.train = self.train.drop(['item', 'name'], axis=1)
+            self.validation['qid'] = self.validation['item'].str.extract(r'(Q\d+)', expand=False)
+            self.validation = self.validation.drop(['item', 'name'], axis=1)
+            self.train['wiki_name'] = self.train['qid'].map(__wiki_name(self.train['qid'].to_list())).fillna(0)
+            self.validation['wiki_name'] = self.validation['qid'].map(__wiki_name(self.validation['qid'].to_list())).fillna(0)
+
+            self.train.to_csv(self._cache.joinpath('base_train.csv'), sep='\t')
+            self.validation.to_csv(self._cache.joinpath('base_validation.csv'), sep='\t')
+
+                    
+               
 
     # Hidden function that recursively appends the new features through a series of if
-    def __produce(self, dataset: pd.DataFrame, encode: bool) -> pd.DataFrame:
+    def __produce(self, dataset: pd.DataFrame, enable_feature:list[str], targe_feature:str, batch_s:int = 1, encode:bool= True) -> pd.DataFrame:
 
         prc_result = pd.DataFrame()
         exstra = []
 
         # Copia diretta delle colonne esistenti nel dataset
-        for feature in tqdm(self.features_enable, desc="copy dataset"):
+        for feature in tqdm(enable_feature, desc="copy dataset"):
            
             if feature == 'G':
-                for c in self.__gf:
+                for c in self.sgf:
                         prc_result.insert(0, c, None)
             else:
                 prc_result.insert(0, feature, None)
 
             if feature in dataset.columns.tolist():
-                if encode and not(feature in self._id) and (
+                if encode and not(feature in self.id) and (
                     dataset[feature].dtype == pd.StringDtype()
                     or dataset[feature].dtype == object
                 ):
-                    if feature == self.target:
+                    if feature == targe_feature:
                         prc_result[feature] = self.label_e.fit_transform(dataset[feature])
                     else:
                         
@@ -64,7 +110,6 @@ class CU_Dataset_Factory:
                             dataset[feature], dtype=pd.Int32Dtype(), prefix=feature
                         )
                         
-                            
                         prc_result = prc_result.drop(feature, axis=1)
                         prc_result = pd.concat([prc_result, dummies], axis=1)
 
@@ -76,7 +121,7 @@ class CU_Dataset_Factory:
         # Elaborazione batch
         batch_cc = 0
         t = tqdm(desc="batch compute", total=len(dataset))  # usa len(dataset) invece di dataset.size
-        for batch in batch_generator(dataset, batch_size=self.batch_size):  # type: ignore
+        for batch in batch_generator(dataset, batch_size=batch_s):  # type: ignore
             batch_cc += 1
             t.set_postfix({"batch": batch_cc})
             
@@ -84,26 +129,26 @@ class CU_Dataset_Factory:
 
             for feature in exstra:
                 t.set_description(feature, refresh=True)
-                
-                if feature == "reference":
-                    
+                if feature == "reference":    
                     batch = count_references(batch.copy(), self.conn)
                 elif feature == "languages":
                     batch = dominant_langs(batch.copy(), self.conn)
                 elif feature == "length_lan":
                     batch = langs_length(batch.copy(), self.conn)
                 elif feature == 'G':
-                    mask = list(self.__gf)
-                    batch = G_factor(batch.copy(), 3, 15, 0.50)
+                    mask = list(self.sgf)
+                    batch = G_factor(batch.copy(), 10, 300, 50, threads=1)
                     prc_result.loc[batch.index, mask] = batch[mask]
-                    continue
+                    continue # add new features  this ...
+                elif feature == 'n_mod': # contare il numero medio di modifiche in un intervallo di tempo specifico
+                    pass
+                elif feature == 'n_visits': # contare il numero medio di visite al giorno in un intervallo di tempo
+                    pass 
                 else:
                     raise ValueError(f"Label:{feature} not valid")
 
                 
                 prc_result.loc[batch.index, feature] = batch[feature]
-
-            # Spostato qui per assicurare che venga aggiornato una volta per batch
             
             t.update(original_batch_len)
 
@@ -111,7 +156,7 @@ class CU_Dataset_Factory:
         return prc_result
 
     # Function that calls back __produce and returns the new dataset
-    def produce(self, out_dir: path.PosixPath, encoding: bool = False, train: bool = True) -> pd.DataFrame:
+    def produce(self, out_dir: path.PosixPath, enable_feature:list[str], targe_feature:str, batch_s:int = 1, encoding: bool = False, train: bool = True) -> pd.DataFrame:
         """
         Transforms Cultural dataset in new dataset with additional features or with a subset of features
         """
@@ -121,49 +166,22 @@ class CU_Dataset_Factory:
             os.mkdir(out_dir)
 
         if train:  # some cases need training set
-            prc_train = self.__produce(self.train, encoding)
+            prc_train = self.__produce(self.train, enable_feature, targe_feature, batch_s, encoding)
             prc_train.to_csv(out_dir.joinpath("train.tsv"), sep="\t", mode="w")
             product = prc_train
         else:
-            prc_validation = self.__produce(self.validation, encoding)
+            prc_validation = self.__produce(self.validation, enable_feature, targe_feature, batch_s, encoding)
             prc_validation.to_csv(
                 out_dir.joinpath("validation.tsv"), sep="\t", mode="w"
             )
             product = prc_validation
 
         return product
-
-    # Returns True if the train or the validation sets exist
-    def exists(self, train: bool) -> bool:
-        return (not train and path.Path("./validation.tsv").exists()) or (
-            train and path.Path("./train.tsv").exists()
-        )
-
-    # Loads the new dataset, returns error if no dataset exists
-    def load(self, train: bool = True) -> pd.DataFrame:
-        result = None
-        if train:
-            if path.Path("./train.tsv").exists():
-                result = pd.read_csv("./train.tsv", sep="\t").drop(
-                    columns=["Unnamed: 0"]
-                )
-            else:
-                raise FileNotFoundError(
-                    "dataset not found, please use produce() before"
-                )
-        else:
-
-            if path.Path("./validation.tsv").exists():
-                result = pd.read_csv("./validation.tsv", sep="\t").drop(
-                    columns=["Unnamed: 0"]
-                )
-            else:
-                raise FileNotFoundError(
-                    "dataset not found, please use produce() before"
-                )
-
-        return result
-
     def produce_one_entry(self, entry: pd.Series, encoding: bool = False) -> pd.DataFrame:
-        result = self.__produce(pd.DataFrame(entry), encoding)
-        return result
+        pass
+
+
+if __name__ == '__main__':
+    d = CU_Dataset_Factory(load=True)
+    print(d.validation.head(10))
+    d.produce(out_dir=path.PosixPath('.'), batch_s=10, enable_feature=['label', 'wiki_name', 'qid', 'languages'],targe_feature='label', train=False)
